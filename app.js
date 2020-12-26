833f71ecb9a5336cecd2762bd537647cd4afa859
masud309060 <masud306090@gmail.com>
2020-12-27 01:06:44 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const logger = require('morgan');
+require('dotenv').config();
+const cors = require('cors')
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
